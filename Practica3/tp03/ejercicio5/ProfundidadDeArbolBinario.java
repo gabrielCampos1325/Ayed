80c1428b227a3908f168763a3a9d58a7f946a80a
Gabriel Alberto Campos <90431287+gabrielCampos1325@users.noreply.github.com>
2023-04-03 19:38:47 -0300

    consulta profundidad de arbol

--- Practica3/tp03/ejercicio5/ProfundidadDeArbolBinario.java
+++ Practica3/tp03/ejercicio5/ProfundidadDeArbolBinario.java
@@ -2,7 +2,7 @@ package tp03.ejercicio5;
 import tp03.ejercicio1.*;
 
 public class ProfundidadDeArbolBinario {
-	private ArbolBinario<Integer> ab = new ArbolBinario<Integer>();
+	private ArbolBinario<Integer> ab;
 	
 	public int sumaElementosProfundidad(int p) {		
 		return sumaRecursiva(ab,p);
@@ -14,8 +14,29 @@ public class ProfundidadDeArbolBinario {
 		}
 		if (p == 0) {
 			return a.getDato();
-		}		
-		return sumaRecursiva(a.getHijoDerecho(), p--) + 
-			   sumaRecursiva(a.getHijoDerecho(), p--);
+		}
+		int p -= 1;		
+		return sumaRecursiva(a.getHijoDerecho(), p) + 
+			   sumaRecursiva(a.getHijoIzquierdo(), p);
+	}	
+}
+
+public class ProfundidadDeArbolBinario {
+	private ArbolBinario<Integer> a;
+	
+	public ProfundidadDeArbolBinario(ArbolBinario<Integer> unArbol){
+		a = unArbol;
+	}
+
+	private int sumaRecursiva(int p) {
+		if (a.esVacio()) {
+			return 0;
+		}
+		if (p == 0) {
+			return a.getDato();
+		}
+		p --;		
+		return new ProfundidadDeArbolBinario(this.getHijoDerecho()).sumaRecursiva(p) 
+		    +  new ProfundidadDeArbolBinario(this.getHijoIzquierdo()).sumaRecursiva(p);
 	}	
 }
