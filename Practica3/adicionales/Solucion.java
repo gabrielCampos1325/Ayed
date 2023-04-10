ec3f0d1134feebb00f5749f5b46965cd7abd47dc
Gabriel Alberto Campos <90431287+gabrielCampos1325@users.noreply.github.com>
2023-04-10 19:08:59 -0300

    Update Solucion.java
    nueva implementacion en clase

--- Practica3/adicionales/Solucion.java
+++ Practica3/adicionales/Solucion.java
@@ -61,5 +61,47 @@ public class Solucion {
 		
 		return (izquierdo.tamanio() > derecho.tamanio() ? izquierdo : derecho);		
 	}
-	 
+
+	
+	public static class Resultado {
+		ListaGenerica<Integer> datos;
+		int total;
+		public Resultado() {
+			datos = new ListaEnlazadaGenerica<>();
+			total = 0;
+		}
+		public void agregar(Integer valor) {
+			total += valor;
+			datos.agregarAlInicio(valor);
+		}
+		public int compareTo(Resultado otro) {
+			if (this.datos.tamanio() < otro.datos.tamanio()) {
+				return -1;  // el objeto resultado que recibe el mensaje es mas chico
+			}
+			if (this.datos.tamanio() > otro.datos.tamanio()) {
+				return 1;  // el objeto resultado que recibe el mensaje es mas grande
+			}
+			return this.total - otro.total;  // tiene la misma cantidad de elemntos, decido por su total
+		}
+	}
+	
+
+	public static ListaGenerica<Integer> caminoMasLargoYMayorValor2(ArbolBinario<Integer> arbol) {
+		if (arbol == null) {
+			return new Resultado();
+		}
+		Resultado r1 = caminoMasLargoYMayorValor2(arbol.getHijoIzquierdo());
+		Resultado r1 = caminoMasLargoYMayorValor2(arbol.getHijoDerecho());
+		Resultado total;
+		
+		if (r1.compareTo(r2) < 0) {
+			total = r2
+		}
+		else {
+			total = r1;
+		}
+		total.agregar(arbol.getDato());
+		return total;
+	}
+
 }
